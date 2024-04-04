cf56f9f453390c8e7be12c870d2a0313e4b1e88e
Finn <tptkds12@gmail.com>
2024-04-04 13:58:58 +0900

    ListHeader 스티키 적용

--- src/app/cart/components/CartList.tsx
+++ src/app/cart/components/CartList.tsx
@@ -5,7 +5,10 @@ import CartListHeader from './cartList/CartListHeader';
 const CartList: React.FC<CartProps> = ({ cartItems }) => {
   return (
     <>
-      <div className="flex items-center justify-between mb-4">
+      <div
+        className="flex items-center justify-between mb-2 sticky bg-zinc-50 shadow py-2 px-2 z-20 rounded"
+        style={{ top: '75px' }}
+      >
         <CartListHeader cartItems={cartItems} />
       </div>
       <ul className="flex flex-col">
--- src/app/cart/components/cartList/CartListHeader.tsx
+++ src/app/cart/components/cartList/CartListHeader.tsx
@@ -1,6 +1,6 @@
 import { CartProps } from '@/types/globalTypes';
-import DeleteSelectedButton from './tableHeader/DeleteSelectedButton';
-import ToggleAllSelectionButton from './tableHeader/ToggleAllSelectionButton';
+import DeleteSelectedButton from './cartListHeader/DeleteSelectedButton';
+import ToggleAllSelectionButton from './cartListHeader/ToggleAllSelectionButton';
 
 const CartListHeader: React.FC<CartProps> = ({ cartItems }) => {
   return (
