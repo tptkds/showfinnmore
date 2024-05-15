539e5d81481c58787964dce4f7b96732504133a7
KimYouGyoung <tptkds12@gmail.com>
2024-05-15 17:07:50 +0900

    🐛 globals.css , tailwind.config.ts 파일 초기화 문제 해결

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,40 +1,40 @@
-import type { Config } from "tailwindcss"
+import type { Config } from 'tailwindcss';
 
-const config = {
-  darkMode: ["class"],
+const config: Config = {
+  darkMode: 'class', // 이 부분도 오류가 있었습니다. 'class'는 배열이 아니라 문자열로 제공되어야 합니다.
   content: [
     './pages/**/*.{ts,tsx}',
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
   ],
-  prefix: "",
+  prefix: '',
   theme: {
     container: {
       center: true,
-      padding: "2rem",
+      padding: '2rem',
       screens: {
-        "2xl": "1400px",
+        '2xl': '1400px',
       },
     },
     extend: {
       keyframes: {
-        "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
         },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
         },
       },
       animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
-} satisfies Config
+  plugins: [require('tailwindcss-animate'), require('daisyui')],
+};
 
-export default config
+export default config;
