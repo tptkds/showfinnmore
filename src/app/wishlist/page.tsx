44e07d90d1f6211bf01e7db09dc513c1bf721aca
KimYouGyoung <tptkds12@gmail.com>
2024-05-15 20:12:09 +0900

    찜 리스트 url로 접근할 때 루트 페이지로 강제 이동 기능 추가

--- src/app/wishlist/page.tsx
+++ src/app/wishlist/page.tsx
@@ -1,8 +1,18 @@
 'use client';
-import React from 'react';
+import React, { useEffect } from 'react';
 import List from './components/List';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
 
 const WishlistComponent: React.FC = () => {
+  const { status } = useSession();
+  const router = useRouter();
+  useEffect(() => {
+    if (status === 'unauthenticated') {
+      alert('로그인 후 이용 가능합니다.');
+      router.push('/');
+    }
+  }, [status]);
   return (
     <div className="pb-24">
       <div className="mt-14 flex flex-col justify-center w-full items-center">
